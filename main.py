0195a2bfb4856f4dee143ea3225a0cb3c83d0d91
Kvidosamsa <159123624+Kvidosamsa@users.noreply.github.com>
2025-01-29 10:58:09 +0100

    change_number1

--- main.py
+++ main.py
@@ -3,13 +3,23 @@ pole = [
     [4, 5, 6],
     [7, 8, 9]
 ]
+
+
+print("Původní pole:")
 for radek in pole:
     print(radek)
-pole.append([100, 110, 120, 130]) 
+
+
+pole[1][1] = 105
+
+
+pole.append([100, 110, 120, 130])
 
 
 for radek in pole:
-    radek.append(0) 
+    radek.append(0)
 
+
+print("Upravené pole:")
 for radek in pole:
     print(radek)
